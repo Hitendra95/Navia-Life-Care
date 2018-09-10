94abc27c125f81fad7eefab7cbc0001e5a83fbaf
Hitendra Dubey <hd.hitendradubey@gmail.com>
2018-09-10 12:37:33 +0530

    Initial Commit

---
+++ Navia Life Care/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navia Life Care
+//
+//  Created by Hitendra Dubey on 10/09/18.
+//  Copyright © 2018 Hitendra Dubey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
